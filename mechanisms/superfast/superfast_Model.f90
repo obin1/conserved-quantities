6ab9f14e3bf4fab24ef90880065ea6974cce93c1
obin1 <obin1@users.noreply.github.com>
2025-11-06 22:31:16 -0800

    species-reaction graph edgelists for 3 mechanisms

---
+++ mechanisms/superfast/superfast_Model.f90
@@ -0,0 +1,21 @@
+MODULE superfast_Model
+
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!  Completely defines the model superfast
+!    by using all the associated modules
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  USE superfast_Precision
+  USE superfast_Parameters
+  USE superfast_Global
+  USE superfast_Function
+  USE superfast_Integrator
+  USE superfast_Rates
+  USE superfast_Jacobian
+  USE superfast_Stoichiom
+  USE superfast_LinearAlgebra
+  USE superfast_Monitor
+  USE superfast_Util
+
+END MODULE superfast_Model
+
