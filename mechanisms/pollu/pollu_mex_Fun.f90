59b6adfb7547e02a64023d1bc9d1fc605d66f06b
obin1 <obin1@users.noreply.github.com>
2026-01-06 13:56:57 -0800

    two small mechanisms from the previous century

---
+++ mechanisms/pollu/pollu_mex_Fun.f90
@@ -0,0 +1,45 @@
+
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ SUBROUTINE mexFunction( nlhs, plhs, nrhs, prhs )
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!                 Matlab Gateway for the Derivative Function Fun
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ USE pollu_Model
+
+      INTEGER nlhs, nrhs
+      INTEGER plhs(*), prhs(*)
+      INTEGER mxGetPr, mxCreateFull, mxGetM, mxgetN
+      INTEGER VPtr, FPtr, RPtr, VdotPtr
+      REAL(kind=dp) V(20), F(0), RCT(25)
+      REAL(kind=dp) Vdot(20)
+
+! Check for the right number of input arguments
+      IF ( nrhs .ne. 3 ) THEN
+         CALL mexErrMsgTxt('Fun requires 3 input vectors: &
+     &V(20), F(0), RCT(25)')
+      END IF 
+! Check for the right number of output arguments
+      IF ( nlhs .ne. 1 ) THEN
+         CALL mexErrMsgTxt('Fun requires 1 output vector: &
+     &Vdot(20)')
+      END IF 
+
+      plhs(1) = mxCreateDoubleMatrix(20,1,0)
+
+      VPtr = mxGetPr(prhs(1))
+      CALL mxCopyPtrToReal8(VPtr,V,20)
+      
+      FPtr = mxGetPr(prhs(2))
+      CALL mxCopyPtrToReal8(FPtr,F,0)
+      
+      RPtr = mxGetPr(prhs(3))
+      CALL mxCopyPtrToReal8(RPtr,RCT,25)
+
+      VdotPtr = mxGetPr(plhs(1))
+
+      CALL Fun( V, F, RCT, Vdot )
+
+      CALL mxCopyReal8ToPtr(Vdot, VdotPtr, 20)
+
+ END SUBROUTINE mexFunction
