aacb0b857f82a689f3f834650719d6dd60c6510a
obin1 <obin1@users.noreply.github.com>
2026-01-09 12:21:48 -0800

    added geoschem hg

---
+++ algorithm_codes/geoschem_hg.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from algorithm import create_sparse, create_coproduction, create_symbols, merge_coprod, s_linalg, linalg_experiment
+
+SEED = 42
+np.random.seed(SEED)
+
+num_experiments = 10
+
+edge_list_gchg= pd.read_csv("../mechanisms/geoschem-hg/Hg_EdgeList.csv", comment="!")
+print("creating sparse matrices...")
+Sr_sparse_gchg, Sp_sparse_gchg, Svv_sparse_gchg= create_sparse(edge_list_gchg)
+print("computing dimension of nullspace...")
+# stoichiometric_invariants_gchg= len(Svv_sparse_gchg.T.nullspace())
+Svv_gchg_np = np.array(Svv_sparse_gchg, dtype=float)
+rank_Svv_gchg_np = np.linalg.matrix_rank(Svv_gchg_np)
+dim_leftnull_gchg= Svv_sparse_gchg.shape[0] - rank_Svv_gchg_np
+
+
+print("identifying coproduction columns...")
+coproduction_cols_gchg= create_coproduction(Sr_sparse_gchg)
+print("creating symbolic dictionary...")
+symbol_dict_gchg= create_symbols(coproduction_cols_gchg)
+print("merging coproduction columns...")
+S_merge_gchg, col_del_gchg= merge_coprod(Sr_sparse_gchg, Sp_sparse_gchg, symbol_dict_gchg, coproduction_cols_gchg, Svv_sparse_gchg)
+print("performing linear algebra...")
+
+del_r_gchg= S_merge_gchg.shape[1] - Svv_sparse_gchg.shape[1]
+    
+rank_list_gchg= linalg_experiment(S_merge_gchg, num_experiments)
+
---
+++ mechanisms/geoschem-hg/Hg_Model.F90
@@ -0,0 +1,21 @@
+MODULE Hg_Model
+
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!  Completely defines the model Hg
+!    by using all the associated modules
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  USE Hg_Precision
+  USE Hg_Parameters
+  USE Hg_Global
+  USE Hg_Function
+  USE Hg_Integrator
+  USE Hg_Rates
+  USE Hg_Jacobian
+  USE Hg_Stoichiom
+  USE Hg_LinearAlgebra
+  USE Hg_Monitor
+  USE Hg_Util
+
+END MODULE Hg_Model
+
